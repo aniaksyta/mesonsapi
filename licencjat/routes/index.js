e6a1b172eda6801c25ae3e396fcfb02ff2b3e18c
Anna <ksytaa@gmail.com>
2015-05-19 18:42:57 +0200

    initial state

---
+++ licencjat/routes/index.js
@@ -0,0 +1,12 @@
+﻿var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/test", { native_parser : true });
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    db.collection('mesons').find().toArray(function (err, items) {
+        res.json(items);
+    });
+    
+};
